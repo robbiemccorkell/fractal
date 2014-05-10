248c734ec40f4e5c672bb39093b668cd5c8a3e4b
Robbie McCorkell <robbie@presentthinking.net>
2014-05-10 22:23:42 +0100

    Initial commit

---
+++ static/js/project.js
@@ -0,0 +1,49 @@
+angular.module('todo', ['ngRoute', 'firebase'])
+
+.value('fbURL', 'https://fractal.firebaseio.com/')
+
+.factory('Todos', function($firebase, fbURL) {
+	return $firebase(new Firebase(fbURL));
+})
+
+.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		controller:'TodoCtrl',
+		templateUrl:'/view/list.html'
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
+})
+
+.controller('ListCtrl', function($scope, Projects) {
+	$scope.projects = Projects;
+})
+
+.controller('TodoCtrl', function ($scope, $location, $timeout, Todos) {
+	$scope.todos = Todos;
+	
+	$scope.addTodo = function() {
+		$scope.todo.done = false;
+		$scope.todos.$add($scope.todo, function() {
+			$timeout(function() { $location.path('/'); });
+		});
+	};
+
+	$scope.remaining = function() {
+		var count = 0;
+		angular.forEach($scope.todos, function(todo) {
+			count += todo.done ? 0 : 1;
+		});
+		return count;
+	};
+
+	$scope.archive = function() {
+		var oldTodos = $scope.todos;
+		$scope.todos = [];
+		angular.forEach(oldTodos, function(todo) {
+			if (!todo.done) $scope.todos.push(todo);
+		});
+	};
+})
